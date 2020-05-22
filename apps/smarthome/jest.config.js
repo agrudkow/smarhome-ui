6aee2a119a687d93176126608d5905171300fed6
agrudkow <agrudkow@gmail.com>
2020-05-23 00:55:49 +0200

    test: fix test

--- apps/smarthome/jest.config.js
+++ apps/smarthome/jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   name: 'smarthome',
   preset: '../../jest.config.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest'
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/apps/smarthome'
+  coverageDirectory: '../../coverage/apps/smarthome',
 };
