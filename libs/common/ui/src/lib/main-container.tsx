534b3c47ec03c5eb502ffc27b5982428249b7453
Artur Grudkowski <agrudkow@gmail.com>
2020-09-21 14:09:20 +0200

    fix: fix sidebar overlay in mobile view

--- libs/common/ui/src/lib/main-container.tsx
+++ libs/common/ui/src/lib/main-container.tsx
@@ -1,23 +1,38 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 
-const StyledMainContainer = styled.div`
-  /* border: 2px solid red; */
+const StyledMainContainer = styled.div<{ dissableScroll: boolean }>`
   min-height: 100%;
   margin: 0;
   padding: 5px;
+  overflow: ${({ dissableScroll }) => (dissableScroll ? 'hidden' : 'unset')};
+  height: ${({
+    dissableScroll,
+    theme: {
+      layout: { headerHeight },
+    },
+  }) => (dissableScroll ? `calc(100vh - ${headerHeight + 10}px)` : 'unset')};
 
   ${(props) => props.theme.breakpoints.tablet} {
     padding: 10px;
+    overflow: unset;
+    height: unset;
   }
 
-  ${(props) => props.theme.breakpoints.tablet} {
+  ${(props) => props.theme.breakpoints.desktop} {
     padding: 20px;
   }
 `;
 
-export const MainContainer: FC = ({ children }) => {
-  return <StyledMainContainer>{children}</StyledMainContainer>;
+export const MainContainer: FC<{ dissableScroll: boolean }> = ({
+  children,
+  dissableScroll,
+}) => {
+  return (
+    <StyledMainContainer dissableScroll={dissableScroll}>
+      {children}
+    </StyledMainContainer>
+  );
 };
 
 export default MainContainer;
